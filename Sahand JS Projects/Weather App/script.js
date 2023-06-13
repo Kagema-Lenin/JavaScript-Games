eaeea47f7d9a85ab59e89fe21ce66e3ad0186abc
Kagema <leninkagema72@gmail.com>
2023-06-13 18:44:24 +0300

    Weather patch

--- Sahand JS Projects/Weather App/script.js
+++ Sahand JS Projects/Weather App/script.js
@@ -2,7 +2,7 @@ const apiKey = "5e7a02196eb399d2c330f22bd746d6db";
 
 const weatherData = document.getElementById('weather-data');
 const cityInput = document.getElementById('city-input');
-const myForm = querySelector('form');
+const myForm = document.querySelector('form');
 
 myForm.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -12,28 +12,32 @@ myForm.addEventListener('submit', (event) => {
 
 async function getWeatherData(cityValue) {
   try {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apiKey}$units=metric`);
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${apiKey}&q=${cityValue}&$units=metric`);
     
     if (!response.ok) { // If the response is not okay: gives any error.
       throw new Error("Network response was not okay");
     }
 
     const data = await response.json();
-    const temperature = Math.round(data.main.temp);
-    const description = data.weather[0].description;
-    const icon = data.weather[0].icon;
+    const temperature = Math.round(data.list[26].main.temp);
+    const description = data.list[26].weather[0].description;
+    const icon = data.list[26].weather[0].icon;
 
     const details = [
-      `Feels like: ${Math.round(data.main.feels_like)}`,
-      `Humidity: ${data.main.humidity}%`,
-      `Wind speed: ${data.wind.speed} m/s`
+      `Feels like: ${Math.round(data.list[26].main.feels_like)}`,
+      `Humidity: ${data.list[26].main.humidity}%`,
+      `Wind speed: ${data.list[26].wind.speed} m/s`
     ];
 
     weatherData.querySelector('.icon').innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}.png" alt="Weather Icon" />`;
-    weatherData.querySelector('.temperature').textContent = `${temperature} C`;
+    weatherData.querySelector('.temperature').textContent = `${temperature}°C`;
     weatherData.querySelector('.description').textContent = `${description}`;
     weatherData.querySelector('.details').innerHTML = details.map((detail) => `<div>${detail}</div>`).join('');
 
   } catch (error) {
+    weatherData.querySelector('.icon').innerHTML = "";
+    weatherData.querySelector('.temperature').textContent = "";
+    weatherData.querySelector('.description').textContent = "Error detected. Please try again later.";
+    weatherData.querySelector('.details').innerHTML = "";
   }
 }
